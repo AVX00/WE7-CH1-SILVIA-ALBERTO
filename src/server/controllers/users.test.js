ae333e185a4a69da550618960c1e8a927708f375
Silvia Martinez <sil@silvia.com>
2022-02-23 19:29:13 +0100

    Added another login test

--- src/server/controllers/users.test.js
+++ src/server/controllers/users.test.js
@@ -32,5 +32,22 @@ describe("Given a login user controller", () => {
 
       expect(res.json).toHaveBeenCalledWith({ token });
     });
+    test("Then if the username or the password it should return an error", async () => {
+      const req = {
+        body: {
+          name: "Pepe",
+          password: "1234",
+        },
+      };
+      const user = req.body;
+      const next = jest.fn();
+      const error = new Error("Incorrect password or username");
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockResolvedValue(false);
+
+      await userLogin(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
   });
 });
