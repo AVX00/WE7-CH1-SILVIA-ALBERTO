d2f70d5f67f9295aade41780a4000e20dae8b7d0
Silvia Martinez <sil@silvia.com>
2022-02-24 00:50:03 +0100

    Addes in index midlewares errors

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
+const { notFound, generalError } = require("./middlewares/errors");
 const platformRouter = require("./routers/platformsRouter");
 const usersRouter = require("./routers/usersRouter");
 
@@ -12,4 +13,7 @@ app.use(express.json());
 
 app.use("/users", usersRouter);
 app.use("/platforms", platformRouter);
+
+app.use(notFound);
+app.use(generalError);
 module.exports = app;
