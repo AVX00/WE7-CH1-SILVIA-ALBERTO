f0ac1ec0a0db547a5f01098ff3a0baf4b4cd32b1
Silvia Martinez <sil@silvia.com>
2022-02-23 18:29:37 +0100

    Created userLogin

---
+++ src/server/controllers/users.js
@@ -0,0 +1,30 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../../database/models/User");
+
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    const error = new Error("Incorrect password or username");
+    error.code = 401;
+    next(error);
+  }
+  const isRightPassword = await bcrypt.compare(password, user.password);
+  if (!isRightPassword) {
+    const error = new Error("Incorrect password or username");
+    error.code = 401;
+    next(error);
+  }
+  const userData = {
+    name: user.name,
+    id: user.id,
+  };
+  const token = jwt.sign(userData, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  return res.json({ token });
+};
+
+module.exports = userLogin;
