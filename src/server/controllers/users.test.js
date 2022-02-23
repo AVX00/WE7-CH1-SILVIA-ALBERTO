1595057c210e8436e3439fd10c2cadf3bf1091af
Silvia Martinez <sil@silvia.com>
2022-02-23 19:17:23 +0100

    Added login test

---
+++ src/server/controllers/users.test.js
@@ -0,0 +1,36 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../../database/models/User");
+const userLogin = require("./usersController");
+
+jest.mock("../../database/models/User");
+
+describe("Given a login user controller", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  describe("When it recives a response", () => {
+    test("Then if the user exist should receives a token", async () => {
+      const res = {
+        json: jest.fn(),
+      };
+
+      const req = {
+        body: {
+          name: "Pepe",
+          password: "1234",
+        },
+      };
+      const token = "token";
+
+      const user = req.body;
+
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      jwt.sign = jest.fn().mockReturnValue(token);
+      await userLogin(req, res);
+
+      expect(res.json).toHaveBeenCalledWith({ token });
+    });
+  });
+});
