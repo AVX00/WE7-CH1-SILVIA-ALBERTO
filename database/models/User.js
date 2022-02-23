037ab398d9f76db9a618cb4389f96eff6f6871f1
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-23 17:30:08 +0100

    referenced user series to series

--- database/models/User.js
+++ database/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   admin: { type: Boolean },
-  series: { type: Array },
+  series: [{ type: Schema.Types.ObjectId, ref: "user" }],
 });
 
 const User = model("user", UserSchema, "users");
