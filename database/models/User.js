6a18cbf9793dd9a1c9b4d8f80f935cb8450b2741
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-23 16:52:46 +0100

    created schema for user

---
+++ database/models/User.js
@@ -0,0 +1,9 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean },
+  series: { type: Array, required: true },
+});
