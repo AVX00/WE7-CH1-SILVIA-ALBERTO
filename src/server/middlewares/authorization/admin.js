f10537bddc8661c471d98f4073970186cf1fc2ee
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-24 19:49:53 +0100

    tested admin middleware

--- src/server/middlewares/authorization/admin.js
+++ src/server/middlewares/authorization/admin.js
@@ -1,15 +1,15 @@
 const User = require("../../../database/models/User");
 
-const admin = (req, res, next) => {
+const admin = async (req, res, next) => {
   try {
-    const user = User.findOne({ id: req.userId });
-
+    const user = await User.findOne({ _id: req.userId });
     if (user.admin) {
       next();
       return;
     }
     res.status(403).json({ error: "permission denied" });
   } catch (error) {
+    error.message = "no user with provided id";
     next(error);
   }
 };
