c1a8abead29e4f44f6e5a885d3e38440e9ce98da
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-24 20:24:18 +0100

    added token to platform tests

--- src/server/routers/platformsRouter.test.js
+++ src/server/routers/platformsRouter.test.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
 const request = require("supertest");
 const app = require("..");
 const connectDB = require("../../database");
 const Platform = require("../../database/models/Platform");
+const User = require("../../database/models/User");
+const { userLogin } = require("../controllers/usersController");
 
 const netflx = {
   name: "netxflx",
@@ -24,9 +27,16 @@ beforeAll(async () => {
 beforeEach(async () => {
   await Platform.create(disney);
   await Platform.create(netflx);
+  const password = await bcrypt.hash("1234", 2);
+  await User.create({
+    name: "joselito",
+    username: "joselit0",
+    password,
+  });
 });
 afterEach(async () => {
   await Platform.deleteMany({});
+  await User.deleteMany({});
 });
 
 afterAll(async () => {
@@ -35,13 +45,38 @@ afterAll(async () => {
 });
 
 describe("Given a platformsRouter", () => {
+  describe("When it receives a get petition at /platforms/ from a not authorized user", () => {
+    test("Then it should respond with status 401 and ", async () => {
+      const endpoint = "/platforms";
+      const expectedStatus = 401;
+      const expectedMessage = { error: "Token Missing" };
+
+      const { body } = await request(app).get(endpoint).expect(expectedStatus);
+
+      expect(body).toEqual(expectedMessage);
+    });
+  });
+
   describe("When it receives a get petition at /platforms/", () => {
     test("Then it should respond with all platforms in the database and status 200", async () => {
       const endpoint = "/platforms";
       const expectedStatus = 200;
       const expectedNumberOfPlatforms = 2;
+      const req = { body: { username: "joselit0", password: "1234" } };
+      let token;
+      const res = {
+        json: (loginToken) => {
+          token = loginToken.token;
+        },
+      };
+      const next = null;
+      await userLogin(req, res, next);
 
-      const { body } = await request(app).get(endpoint).expect(expectedStatus);
+      console.log(token);
+      const { body } = await request(app)
+        .get(endpoint)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(expectedStatus);
 
       expect(body).toHaveLength(expectedNumberOfPlatforms);
     });
